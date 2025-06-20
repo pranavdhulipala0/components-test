a6141a41191a5f0fc48dd1862295f0b57d4643a8
Pranav <pranav.d@eksaq.in>
2025-06-20 12:58:47 +0530

    precompiled library

--- lib/src/components/ButtonComponent.d.ts
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-type MyButtonProps = {
-    title: string;
-    onPress: () => void;
-};
-declare const ButtonComponent: React.FC<MyButtonProps>;
-export default ButtonComponent;
